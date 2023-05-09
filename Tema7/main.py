1e38e4cd315db4f4eaa1c5a9385d86f344298368
Otilia Ghioc <otilia.ghioc@levi9.com>
2023-05-09 14:03:37 +0300

    update tema7

--- Tema7/main.py
+++ Tema7/main.py
@@ -2,7 +2,7 @@ import numpy as np
 import random
 import math
 
-epsilon = pow(10,-8)
+epsilon = pow(10, -8)
 
 
 def citireaPolinomului():
@@ -15,15 +15,16 @@ def citireaPolinomului():
     return np.poly1d(lst), lst
 
 
-def Horner( listaCoeficienti, x):
+def Horner(listaCoeficienti, x):
     bAnterior = listaCoeficienti[0]
-    for i in range (1, len(listaCoeficienti)+1):
+    bCurent = 0
+    for i in range(1, len(listaCoeficienti)):
         bCurent = listaCoeficienti[i] + bAnterior * x
         bAnterior = bCurent
     return bCurent
 
 
-def Semn ( valoare):
+def Semn(valoare):
     if valoare < 0:
         return -1
     return 1
@@ -36,52 +37,79 @@ def calcH(n, p, pd, pdd):
 def metodaLuiLaguerre(listaCoeficienti, R, kMax):
     # x0 == x, x1 == y...
     x = random.uniform(-R, R)
-    #print(x)
+    k = 0
+    # print(x)
     # ne pregatim de Horner
 
-    valPolinomInX = Horner(listaCoeficienti,x)
+
     polinom = np.poly1d(listaCoeficienti)
     derivataI = polinom.deriv(1)
     coefDerivataI = derivataI.coef
 
-    valDerivataI = Horner(coefDerivataI, x)
-    semn = Semn(valDerivataI)
-
     derivataII = derivataI.deriv(1)
     coefDerivataII = derivataII.coef
-    valDerivataII = Horner(coefDerivataII, x)
 
-    px = Horner(listaCoeficienti, x)
+    while True:
+        #calculam delta
+        valPolinomInX = Horner(listaCoeficienti, x)
 
-    H = calcH(len(listaCoeficienti), px, valDerivataI, valDerivataII)
+        valDerivataI = Horner(coefDerivataI, x)
+        semn = Semn(valDerivataI)
 
-    if H < 0 : exit(1)
+        valDerivataII = Horner(coefDerivataII, x)
 
-    numitorDeltaX = (valDerivataI + semn * math.sqrt(H))
-    if -epsilon <= numitorDeltaX <= epsilon : exit(2)
+        H = calcH(len(listaCoeficienti)-1, valPolinomInX, valDerivataI, valDerivataII)
 
+        if H < 0:
+            print("H < 0")
+            exit()
 
-    deltaX = (len(listaCoeficienti) * valPolinomInX) / numitorDeltaX
-    y = x - deltaX
+        #print(H)
+        numitorDeltaX = (valDerivataI + semn * math.sqrt(H))
+        #print(numitorDeltaX, "numitor")
+        if -epsilon <= numitorDeltaX <= epsilon:
+            print("Numitorul din deltaX este in [-epsilon, epsilon]")
+            exit()
 
-    k = 1
-    while abs(deltaX)>= epsilon and k <= kMax and deltaX <= pow(10,8):
+        deltaX = (len(listaCoeficienti) * valPolinomInX) / numitorDeltaX
 
+        x = x - deltaX
+        k = k + 1
 
+        if abs(deltaX) <= epsilon or k >= kMax or deltaX >= pow(10, 8):
+            break
+    if epsilon > abs(deltaX):
+        return x
+    else:
+        print("Divergenta!")
+        exit()
 
 
 if __name__ == '__main__':
-    #polinom, listaCoeficienti = citireaPolinomului()
-    #print(polinom(1))
-    listaCoeficienti = [1, 2, 3, 4, 5]
-
+    # polinom, listaCoeficienti = citireaPolinomului()
+    # print(polinom(1))
+    listaCoeficienti = [1.0, -6.0, 13.0, -12.0, 4.0]
 
     R = (abs(listaCoeficienti[0]) + max(listaCoeficienti)) / abs(listaCoeficienti[0])
     print("Radacinele se afla in intervalul: [", -R, ",", R, "]")
 
+    listaSolutii = list()
+
     # asta de mai multe ori
-    radacina = metodaLuiLaguerre(listaCoeficienti, R, 100)
+    for i in range(0, 10):
+        radacina = metodaLuiLaguerre(listaCoeficienti, R, 100)
+        ok = 1
+        #print(radacina)
+        for j in range(0, len(listaSolutii)):
+            if abs(radacina-listaSolutii[j]) < epsilon:
+                ok = 0
+        if ok == 1:
+            listaSolutii.append(radacina)
 
+    print("Solutiile distincte:")
+    print(listaSolutii)
 
-
-
+    file = open('geek.txt', 'w')
+    for i in range(0, len(listaSolutii)):
+        file.write(f"{listaSolutii[i]}\n")
+    file.close()
